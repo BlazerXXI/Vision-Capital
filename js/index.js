c1c0f7b5553d34e969ec0459f47168766361c3e8
BlazerXXI <ruslandendik@gmail.com>
2023-05-22 00:12:11 +0300

    dev

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  // функциональность кнопки "меню бургер"
+  $("#burgerMenu").click(function () {
+    if ($(this).hasClass("burger-active")) {
+      // Если кнопка имеет класс "burger-active"
+      $(this).removeClass("burger-active"); // Удаляем класс "burger-active" у текущей нажатой кнопки
+      $("#nav").removeClass("nav-active"); // Удаляем класс "nav-active" у элемента с id "nav"
+    } else {
+      // Если кнопка не имеет класса "burger-active"
+      $(this).addClass("burger-active"); // Добавляем класс "burger-active" к текущей нажатой кнопке
+      $("#nav").addClass("nav-active"); // Добавляем класс "nav-active" к элементу с id "nav"
+      $("header").addClass("d-block");
+    }
+  });
+
+	
+});
